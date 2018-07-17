961a8f1468438e8b39ba90b36ed7080ab13e2be0
sunny-sarath-2 <33948748+sunny-sarath-2@users.noreply.github.com>
2018-07-17 11:42:29 +0530

    node and react.js setup
    to use node and react.js at same time use npm run all command

--- server.js
+++ server.js
@@ -49,5 +49,5 @@ app.post("/", (req, res) => {
   }
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
