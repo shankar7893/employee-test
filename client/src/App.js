c96ec92dc0a67ceb017be1b83fc1c95269688e45
shankar <shankar149reddy@gmail.com>
2018-07-17 16:25:47 +0530

    reactjs login screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import './Login/css/style.css'
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+         <div class="body"></div>
+		<div className="grad"></div>
+		<div className="header">
+			<div>Pronteff<span>Login</span></div>
+		</div>
+		<br />
+		<div className="login">
+				<input type="text" placeholder="username" name="user" /><br />
+				<input type="password" placeholder="password" name="password" /><br />
+				<input type="button" value="Login" />
+		</div>
       </div>
     );
   }
