eec01059745690462af78d12cac9e3de722a6345
sunny-sarath-2 <33948748+sunny-sarath-2@users.noreply.github.com>
2018-07-19 11:19:13 +0530

    webpack.config file

---
+++ client/webpack.config.js
@@ -0,0 +1,56 @@
+/* === dont forget to import scss to main.js file === */
+/* ===> import './main.scss'; <=== */
+
+var path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+    publicPath: "/public"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: { presets: ["es2015", "react"] }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader" // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader" // translates CSS into CommonJS
+          },
+          {
+            loader: "sass-loader" // compiles Sass to CSS
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
