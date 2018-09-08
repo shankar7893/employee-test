c9967b53ee58aa259f66088e877c4ac8ba578066
shankar <shankar149reddy@gmail.com>
2018-09-08 11:20:25 +0530

    last

--- mobile/components/SettingsPage.js
+++ mobile/components/SettingsPage.js
@@ -25,93 +25,101 @@ import axios from "react-native-axios";
 import Success from "./Success";
 import AttendenceScreen from "./AttendenceScreen";
 
-  class SettingsPage extends React.Component {
-    constructor(props) {
-        super(props);
-    
-        this.state = {
-          latitude: null,
-          longitude: null,
-          error:null,
-         
-        };
-      }
-      
-     async componentDidMount() {
-        const unique_id =   await AsyncStorage.getItem('uniqueId'); 
-        BackHandler.addEventListener('hardwareBackPress', this.handleBackButton);
-        if(unique_id != null ) {
-          this.props.navigation.navigate('Success');
-        };
-        
-        if(unique_id == null  ){
-           navigator.geolocation.getCurrentPosition(
-         (position) => {
-             if((position.coords.latitude < 17.450926) && (position.coords.latitude > 17.448955) ) {
-              if((position.coords.longitude < 78.388429) && (position.coords.longitude > 78.385987 ) ) {
-                 // Alert.alert('HI','Noted','OK');
-                  this.props.navigation.navigate('Success');
-               
+class SettingsPage extends React.Component {
+  constructor(props) {
+    super(props);
 
-                  
-              }
-              else {
-                this.props.navigation.navigate('AttendenceScreen');
-              }
+    this.state = {
+      latitude: null,
+      longitude: null,
+      error: null,
+      verifyDate: new Date().getDate().toString()
+    };
+  }
+
+  async componentDidMount() {
+    const checkDate = await AsyncStorage.getItem("checkDate");
+
+    BackHandler.addEventListener("hardwareBackPress", this.handleBackButton);
+    if (this.state.verifyDate != checkDate || checkDate == null) {
+      await AsyncStorage.removeItem("uniqueId");
+    } else {
+      let unique_id = await AsyncStorage.getItem("uniqueId");
+
+      if (unique_id != null && this.state.verifyDate == checkDate) {
+        this.props.navigation.navigate("Success");
+      }
+    }
+    let unique_id = await AsyncStorage.getItem("uniqueId");
+    if (unique_id == null) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          if (
+            position.coords.latitude < 17.450926 &&
+            position.coords.latitude > 17.448955
+          ) {
+            if (
+              position.coords.longitude < 78.388429 &&
+              position.coords.longitude > 78.385987
+            ) {
+              // Alert.alert('HI','Noted','OK');
+              this.props.navigation.navigate("Success");
+            } else {
+              this.props.navigation.navigate("AttendenceScreen");
             }
-            else {
-              this.props.navigation.navigate('AttendenceScreen');
-            }
-            
+          } else {
+            this.props.navigation.navigate("AttendenceScreen");
+          }
 
-             this.setState({
-               latitude: position.coords.latitude,
-               longitude: position.coords.longitude,
-               error: null,
-             });
-           },
-           (error) => {
-            this.props.navigation.navigate('AttendenceScreen');
-             this.setState({ error: error.message })},
-           { enableHighAccuracy: false, timeout: 200000, maximumAge: 1000 },
-          );
-        }
-        }
+          this.setState({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            error: null
+          });
+        },
+        error => {
+          this.props.navigation.navigate("AttendenceScreen");
+          this.setState({ error: error.message });
+        },
+        { enableHighAccuracy: false, timeout: 200000, maximumAge: 1000 }
+      );
+    }
+  }
 
-        componentWillUnmount() {
-          BackHandler.removeEventListener('hardwareBackPress', this.handleBackButton);
-      }
-      handleBackButton() {
-        return true;
-      }
-        render() {
-          return (
-            <View style={styles.container}>
-              <ActivityIndicator />
-              <StatusBar barStyle="default"  />
-            </View>
-          );
-        }
-      }
-    //   attendenceAsync = async () => {
-    //   if((this.state.latitude < 17.450926) && (this.state.latitude > 17.448955) ) {
-    //     if((this.state.longitude < 78.388429) && (this.state.longitude > 78.385987 ) ) {
-    //        // Alert.alert('HI','Noted','OK');
-    //         this.props.navigation.navigate('Success');
-    //     }
-    //   }
-    //   else {
-    // //Alert.alert('HI','Sorry you Are Not in office try again in office','OK');
-    // this.props.navigation.navigate('AttendenceScreen');
+  componentWillUnmount() {
+    BackHandler.removeEventListener("hardwareBackPress", this.handleBackButton);
+  }
+  handleBackButton() {
+    return true;
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator />
+        <StatusBar barStyle="default" />
+      </View>
+    );
+  }
+}
+//   attendenceAsync = async () => {
+//   if((this.state.latitude < 17.450926) && (this.state.latitude > 17.448955) ) {
+//     if((this.state.longitude < 78.388429) && (this.state.longitude > 78.385987 ) ) {
+//        // Alert.alert('HI','Noted','OK');
+//         this.props.navigation.navigate('Success');
+//     }
+//   }
+//   else {
+// //Alert.alert('HI','Sorry you Are Not in office try again in office','OK');
+// this.props.navigation.navigate('AttendenceScreen');
 
-    //   }
-    // }
-      _signOutAsync = async () => {
-        await AsyncStorage.clear();
-        this.props.navigation.navigate('Auth');
-      };
-    
-    export default SettingsPage;
+//   }
+// }
+_signOutAsync = async () => {
+  await AsyncStorage.clear();
+  this.props.navigation.navigate("Auth");
+};
+
+export default SettingsPage;
 
 const styles = StyleSheet.create({
   container: {
